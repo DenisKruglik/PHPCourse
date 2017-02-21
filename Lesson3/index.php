1e4736971701f7f8f4229e87b5097a5d3a516b67
DenisKruglik <d.kruglik11011@gmail.com>
2017-02-21 08:53:02 +0300

    Lesson 3

---
+++ Lesson3/index.php
@@ -0,0 +1,35 @@
+<?php
+	// $a=array(1,2,3,4,5,6,7,8,9,10,11,12);
+	// function remainder($item){
+	// 	if ($item%5 < 2) {
+	// 		return true;
+	// 	}
+	// }
+	// $b=array_filter($a, "remainder");
+	// print_r($b);
+
+	// echo time()."<br>".microtime()."<br>".microtime(true)."<br>".date("d/m/Y H:i:s");
+
+	// echo ((strtotime("2034-11-21 00:00:00")-strtotime("1985-04-17 00:00:00"))/3600)."<br>".date("d/m/Y", ((((1<<62)-1)<<1)+1));
+	// echo strtotime("2074-11-21 00:00:00")."<br>".strtotime("1985-04-17 00:00:00");
+
+	// $dir = opendir(".");
+	// while ($d = readdir($dir)) {
+	// 	echo $d."<br>";
+	// }
+
+	// print_r(scandir("."));
+
+	print_r(stat("./index.php"));
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
