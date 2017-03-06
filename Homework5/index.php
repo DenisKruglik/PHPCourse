8304491d4c78821d78d3df9ad52606fe20de6ee5
DenisKruglik <d.kruglik11011@gmail.com>
2017-03-06 10:50:33 +0300

    Fixed some bugs

--- Homework5/index.php
+++ Homework5/index.php
@@ -2,16 +2,22 @@
 	#1
 
 	$num = trim($_GET["num"]); 
-	$exp = "/([375\+]{3,4})\s*\-*(\d{2})\-*\s*(\d{2})\-*\s*(\d{2})\-*\s*(\d{3})/"; 
+	$exp = "/(\+?375)\s*\-*\s*(\d{2})\s*\-*\s*(\d{2,3})\s*\-*\s*(\d{2})\s*\-*\s*(\d{2,3})/"; 
 	$matches = array(); 
 	preg_match_all($exp, $num, $matches);
+	$res = $matches[1][0]."-".$matches[2][0]."-".$matches[3][0]."-".$matches[4][0]."-".$matches[5][0];
 
 	#2
 
 	$input = trim($_GET["input"]); 
-	$string = "/([httpsw]{2,5})\:\/\/([A-Za-z0-9]{3,})\.*([a-z]*)\.([a-z]{2,})\/*([A-Za-z0-9\/]*)\/*\?*([\w\=\-\+\&\%]*)\#*([\w]*)/"; 
+	$string = "/(http|https|ftp|ws)\:\/\/([A-Za-z0-9]{3,})\.*([a-z]*)\.([a-z]{2,})\/*([A-Za-z0-9\/]*)\/*\?*([\w\=\-\+\&\%]*)\#*([\w]*)/"; 
 	$matches1 = array(); 
 	preg_match_all($string, $input, $matches1);
+	if ($matches1[0] != null) {
+		$res1 = "Valid URL";
+	}else{
+		$res1 = "Invalid URL";
+	}
 	?>
 
 <!DOCTYPE html>
@@ -22,14 +28,16 @@
 </head>
 <body>
 	<form>
+		<span>For telephone number</span>
 		<input type="text" name="num">
 		<input type="submit" value="submit">
 	</form>
-	<pre><?php print_r($matches) ?></pre>
+	<pre><?=$res ?></pre>
 	<form>
+		<span>For URL</span>
 		<input type="text" name="input">
 		<input type="submit" value="submit">
 	</form>
-	<pre><?php print_r($matches1) ?></pre>
+	<pre><?=$res1 ?></pre>
 </body>
 </html>
